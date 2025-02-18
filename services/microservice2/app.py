560e582f56973e0aeb1652c6299da287102f7499
root <root@DESKTOP-5UTUF1L>
2025-02-18 08:58:41 +0100

    task update

---
+++ services/microservice2/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Service 2"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
